8a042a48013c65b2c238cdf4351349d133865ed8
VinaLx <vinaleux@gmail.com>
2019-01-20 05:29:09 +0800

    finish rev injection

--- software-foundations/logical-foundations/ch3-lists/listprop.v
+++ software-foundations/logical-foundations/ch3-lists/listprop.v
@@ -117,17 +117,59 @@ Proof.
     reflexivity.
 Qed.
 
-(* unfinished *)
+Theorem snoc_not_empty : forall (x : nat) (xs : natlist),
+    [] = xs ++ [x] -> False.
+Proof.
+    intros x xs eq.
+    induction xs as [| xh xs' H].
+    - simpl in eq. inversion eq.
+    - simpl in eq. inversion eq.
+Qed.
+
+Inductive natlist_pair : natlist -> natlist -> Type :=
+| nils : natlist_pair [] []
+| l_cons : forall (x : nat) (xs : natlist), natlist_pair (x :: xs) []
+| r_cons : forall (y : nat) (ys : natlist), natlist_pair [] (y :: ys)
+| conses : forall (x y : nat) (xs ys : natlist),
+    natlist_pair xs ys -> natlist_pair (x :: xs) (y :: ys).
+
+Fixpoint to_natlist_pair (xs ys : natlist) : natlist_pair xs ys :=
+    match xs, ys with
+    | [], [] => nils
+    | x :: xs, [] => @l_cons x xs
+    | [], y :: ys => @r_cons y ys
+    | x :: xs', y :: ys' => @conses x y xs' ys' (to_natlist_pair xs' ys')
+    end.
+
+Theorem list_snoc_eq : forall (xs ys : natlist) (x y : nat),
+    xs ++ [x] = ys ++ [y] -> xs = ys /\ x = y.
+Proof.
+    intros xs ys x y eq.
+    induction (to_natlist_pair xs ys).
+    - simpl in eq. inversion eq. split; reflexivity.
+    - simpl in eq. inversion eq. symmetry in H1.
+      apply snoc_not_empty in H1. contradiction.
+    - simpl in eq. inversion eq.
+      apply snoc_not_empty in H1. contradiction.
+    - simpl in eq. inversion eq.
+      apply IHn in H1. destruct H1 as [xsys xy]. split.
+      + rewrite xsys. reflexivity.
+      + assumption.
+Qed.
+
 Theorem rev_inject : forall (l1 l2 : natlist),
   rev l1 = rev l2 -> l1 = l2.
 Proof.
   intros l1 l2 rev_eq.
-  induction l1 as [| h1 t1 eq].
-  - destruct l2.
-    + reflexivity.
-    + admit.
-  - destruct l2.
-    + admit.
-    + admit.
-Admitted.
+  induction (to_natlist_pair l1 l2).
+  - reflexivity.
+  - simpl in rev_eq. symmetry in rev_eq. 
+    apply snoc_not_empty in rev_eq. contradiction.
+  - simpl in rev_eq.
+    apply snoc_not_empty in rev_eq. contradiction.
+  - simpl in rev_eq. apply list_snoc_eq in rev_eq.
+    destruct rev_eq as [rxsrys xy].
+    apply IHn in rxsrys.
+    rewrite rxsrys. rewrite xy. reflexivity.
+Qed.
 
