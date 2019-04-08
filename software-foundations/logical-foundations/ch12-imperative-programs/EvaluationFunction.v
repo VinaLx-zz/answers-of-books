5a5936e2a52d7d41e032bdecb26ee9e5654c87f8
VinaLx <vinaleux@gmail.com>
2019-04-08 21:54:44 +0800

    start doing logical foundation chapter 14

---
+++ software-foundations/logical-foundations/ch12-imperative-programs/EvaluationFunction.v
@@ -0,0 +1,7 @@
+(*
+  Sources in this file is the sample solution of chapter 14 of logical
+  foundation: "evaluation function". The source file is placed here since the
+  content of chapter 14 is closely related to the content of chapter 12:
+  "imperative programs".
+*)
+
---
+++ software-foundations/logical-foundations/ch14-evaluation-function/Readme.md
@@ -0,0 +1,2 @@
+The sample solutions of exercises in this chapter is in `../ch12-imperative-programs/evaluate-function.v`, since the content of this chapter is closely related to (and referencing) the Imp language designed in chapter 12.
+
